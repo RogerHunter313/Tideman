311c4685889671ed0fdb6eb2c275f6dc3f9f2e2b
Roger Hunter <rogerhunter313@gmail.com>
2021-05-08 13:09:17 -0500

    build : user input, check for invalid usage, populate candidates list

--- Tideman/Tideman/tideman.cpp
+++ Tideman/Tideman/tideman.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 // Max number of candidates, don't use ; here, causes errors
@@ -25,11 +27,11 @@ string candidates[MAX];
 // we have 9 candidates, each candidate is paired with the other 8 candidates, this counts each pair twice so divide by 2
 pairing pairs[MAX * (MAX - 1) / 2];
 
-int pair_count;
+int pairCount;
 int numCandidates;
 
 // Function prototypes
-bool vote(int rank, string name, int ranks[]);
+bool vote(int rank, string name, vector<int> ranks);
 void record_preferences(int ranks[]);
 void add_pairs(void);
 void sort_pairs(void);
@@ -66,9 +68,63 @@ int main() {
 
 	cout << "You entered there are " << numCandidates << " candidates" << endl;
 
+	string name;
+	
+	for (int i = 0; i < numCandidates; i++) {
+		cout << "Name for candidate " << i + 1 << ": " << flush;
+		cin >> name;
+		cout << "Candidate " << i + 1 << " is " << name << endl;
+		candidates[i] = name;
+	}
+	
+	// Clear graph of locked in pairs
+	for (int i = 0; i < numCandidates; i++) {
+		for (int j = 0; j < numCandidates; j++) {
+			locked[i][j] = false;
+		}
+	}
+
+	// initialize number of pairs and get number of voters from user
+	pairCount = 0;
+	int voterCount;
+	cout << endl;
+	cout << "Number of voters : " << flush;
+	cin >> voterCount;
+
+	// Query for votes
+	for (int i = 0; i < voterCount; i++) {
+
+		// ranks[i] is voter's ith preference
+		// size of vector set to numCandidates, initialized to 0
+		vector<int> ranks(numCandidates, 0);
+
+		cout << "Enter ranks for voter " << i + 1 << endl;
+
+		// Query for each rank
+		for (int j = 0; j < numCandidates; j++) {
+			string name;
+			cout << "Rank " << j + 1 << ": " << flush;
+			cin >> name;
+
+	/*		if (!vote(j, name, ranks)) {
+				cout << "Invalid vote" << endl;
+				return 2;
+			}*/
+		}
+		cout << endl;
+	}
+
+
 	return 0;
 }
 
+// Update ranks given a new vote
+bool vote(int rank, string name, vector<int> ranks) {
+	//for (int i = 0; i < numCandidates; i++) {
+	//	if ()
+	//}
+	return true;
+}
 
 
 
